7384ff3dca24b4e42615d3dd43d0a53c6be4ad2c
Mustaf Hussain <mustafiaaa@gmail.com>
2021-02-03 22:02:43 +0530

    intial setup is done

---
+++ main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const bird = document.querySelector('.bird');
+    const container = document.querySelector('.container');
+    const ground = document.querySelector('.ground');
+});
